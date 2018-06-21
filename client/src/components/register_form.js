8478be697bd383f8b6354d077ed4abc1583a20be
Grizvok <burninglovedesign@gmail.com>
2018-06-21 09:15:25 -0400

    client-side validation

--- client/src/components/register_form.js
+++ client/src/components/register_form.js
@@ -8,14 +8,15 @@ export default class RegisterForm extends React.Component {
     this.state = {
       email: '',
       password: '',
+      isPasswordValid: 'Password is not valid',
       confirmPassword: '',
-      isValid: 'valid'
+      isConfirmPasswordValid: ''
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   };
-  
+
   handleChange(event) {
     const target = event.target;
     const value = event.target.value;
@@ -26,46 +27,48 @@ export default class RegisterForm extends React.Component {
     });
   }
 
+
   handleSubmit(event) {
     event.preventDefault();
-    console.log(this.state);
+    return;
     axios.post('/api/register', {
       email: this.state.email,
       password: this.state.password,
       confirmPassword: this.state.confirmPassword
     })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   render() {
     return (
-			<Container>
-			<Row className="justify-content-center">	
-			<Col xs="4">
-      <Form method="POST" onSubmit={this.handleSubmit}>
-        <FormGroup>
-          <Label for="exampleEmail">Email</Label>
-          <Input valid={this.state.isValid} type="email" value={this.state.email} onChange={this.handleChange} name="email" id="exampleEmail" required/>
-          <FormFeedback valid>Sweet! that name is available</FormFeedback>
-          <FormText>Please enter a valid email.</FormText>
-        </FormGroup>
-        <FormGroup>
-          <Label for="Password">Password</Label>
-          <Input type="password" value={this.state.password} onChange={this.handleChange} name="password" id="Password" required/>
-        </FormGroup>
-				<FormGroup>
-          <Label for="confirmPassword">Confirm Password</Label>
-          <Input type="password" value={this.state.confirmPassword} onChange={this.handleChange} name="confirmPassword" id="confirmPassword" required/>
-        </FormGroup>
-        <Button>Submit</Button>
-      </Form>
-			</Col>
-			</Row>
+      <Container>
+        <Row className="justify-content-center">
+          <Col xs="4">
+            <Form method="POST" onSubmit={this.handleSubmit}>
+              <FormGroup>
+                <Label for="exampleEmail">Email</Label>
+                <Input type="email" value={this.state.email} onChange={this.handleChange} name="email" id="exampleEmail" required />
+                <FormFeedback>Sweet! that name is available</FormFeedback>
+              </FormGroup>
+              <FormGroup>
+                <Label for="Password">Password</Label>
+                <Input type="password" value={this.state.password} onChange={this.handleChange} name="password" id="Password" required />
+                <FormText>{this.state.isPasswordValid}</FormText>
+              </FormGroup>
+              <FormGroup>
+                <Label for="confirmPassword">Confirm Password</Label>
+                <Input type="password" value={this.state.confirmPassword} onChange={this.handleChange} name="confirmPassword" id="confirmPassword" required />
+                <FormFeedback valid>Sweet! that name is available</FormFeedback>
+              </FormGroup>
+              <Button>Submit</Button>
+            </Form>
+          </Col>
+        </Row>
       </Container>
     );
   }
